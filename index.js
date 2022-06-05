9cb51ece35f5fbe4ac7f990fd6d6a0918888fde6
nikkolistio <nikkolistiow@gmail.com>
2022-06-05 21:34:41 +0700

    membuat fungsi sortascending dan descanding

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arr, cbNama) => {
+  sortNama = cbNama(arr);
+  listNama = [];
+  for (let counter = 0; counter < sortNama.length; counter++) {
+    listNama.push((counter + 1).toString() + ". " + sortNama[counter]);
+  }
+
+  return listNama;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arrNama) => {
+  return arrNama.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arrNama) => {
+  return arrNama.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
